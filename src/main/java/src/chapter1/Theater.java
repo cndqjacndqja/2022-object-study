42ccb9b577da75f5e9c0b054aa402e8f9db3fe7c
jochungbeom <cndqjacndqja@gmail.com>
2022-03-22 21:00:35 +0900

    feat(chapter1): 영화관 티켓 판매 기능 구현

---
+++ src/main/java/src/chapter1/Theater.java
@@ -0,0 +1,21 @@
+package src.chapter1;
+
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        if (audience.getBag().hashInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketSeller.getTicketOffice().getTickets();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+}
---
+++ src/main/java/src/chapter1/Ticket.java
@@ -0,0 +1,13 @@
+package src.chapter1;
+
+public class Ticket {
+    private Long fee;
+
+    public Ticket(Long fee) {
+        this.fee = fee;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+}
